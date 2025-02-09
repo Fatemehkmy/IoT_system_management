45cfdc14ae7740cc38fdf2a570bb7e8b660bb303
Fatemehkmy <153651811+Fatemehkmy@users.noreply.github.com>
2025-02-09 11:13:42 +0330

    Add files via upload

--- finalproject_Task1.py
+++ finalproject_Task1.py
@@ -1,15 +1,3 @@
-'''
-APM:
-
-injaro ezafe kardam agar soal dashtid inja benevisid, moafagh bashid
-
-
-
-
-'''
-
-
-
 #A code for a Device that manages the light's exposure and amount of water required for apartment flowers
 
 import time
@@ -23,11 +11,17 @@ class Sensor:
         self.group = self.topic_list[1]
         self.device_type = self.topic_list[2]
         self.name = self.topic_list[3]
+        self.status= 'OFF'
         self.current_value = 0
         self.current_time = 0
         self.start_time = 0
         self.end_time = 0
         self.pin = pin
+        self.report_sensor_status=[]
+
+    def __str__(self):
+        return f'Group_Name: {self.group}, Device_Type: {self.device_type} , Device_Name : {self.name}'
+
 
     def read_sensor(self):
         if self.device_type == 'lights':
@@ -40,17 +34,56 @@ class Sensor:
             self.current_value = np.random.uniform(0, 1)
             return self.current_value
 
+    def turn_on(self):
+        self.status = 'ON'
+        #self.send_command('TURN_ON')
+        if self.device_type == 'Phototransistors':
+            #GPIO.output(56, GPIO.HIGH)
+            print(f'{self.name} was turned on successfully!')
+            self.report_sensor_status.append(f'{self.name} : ON')
+
+        elif self.device_type == 'hygrometer':
+            #GPIO.output(39, GPIO.HIGH)
+            print(f'{self.name} was turned on successfully!')
+            self.report_sensor_status.append(f'{self.name} : ON')
+
+
+
+    def turn_off(self):
+        self.status = 'OFF'
+        #self.send_command('TURN_OFF')
+        if self.device_type == 'Phototransistors':
+            #GPIO.output(56, GPIO.LOW)
+            print(f'{self.name} was turned off successfully!')
+            self.report_sensor_status.append(f'{self.name} : OFF')
+
+        elif self.device_type== 'hygrometer':
+            #GPIO.output(39, GPIO.LOW)
+            print(f'{self.name} was turned off successfully!')
+            self.report_sensor_status.append(f'{self.name} : OFF')
+
+
 
 class Device(Sensor):
-    def __init__(self, topic, mqtt_broker='local_host', port=1883):
+    def __init__(self, topic, mqtt_broker='local_host', port=1883, status = 'OFF', power_rating=0):
         super().__init__(topic)
-        self.status= 'off'
+        self.status= status
+        self.power_rating = power_rating
+        self.hours_on = 0
+        self.start_time = 0
         self.port= port
         self.mqtt_broker= mqtt_broker
         self.lights_status=[]
         self.waters_status =[]
         #self.connect_mqtt()
         #self.setup_gpio()
+
+    def __str__(self):
+        parent_str = super().__str__()
+        return f'{parent_str}, Status: {self.status}'
+
+    def __repr__(self):
+        return self.__str__()
 
     def setup_gpio(self):
         if self.device_type == 'lights':
@@ -65,6 +98,7 @@ class Device(Sensor):
         self.status = 'on'
         #self.send_command('TURN_ON')
         if self.device_type == 'lights':
+            self.start_time = time.time()
             #GPIO.output(17, GPIO.HIGH)
             print(f'{self.name} was turned on successfully!')
             self.lights_status.append(f'{self.name} : ON')
@@ -79,11 +113,14 @@ class Device(Sensor):
         self.status = 'off'
         #self.send_command('TURN_OFF')
         if self.device_type == 'lights':
+            total_time = time.time() - self.start_time
+            self.hours_on += total_time / 3600
+            self.start_time = None
             #GPIO.output(17, GPIO.LOW)
             print(f'{self.name} was turned off successfully!')
             self.lights_status.append(f'{self.name} : OFF')
 
-        elif self.device_type == 'waters':
+        elif self.device_type== 'waters':
             #GPIO.output(44, GPIO.LOW)
             print(f'{self.name} was turned off successfully!')
             self.waters_status.append(f'{self.name} : OFF')
@@ -127,6 +164,15 @@ class Device(Sensor):
             print('The history of waters status is:')
             for status in self.waters_status:
                 print(status)
+        elif self.device_type == 'Phototransistors':
+            print('The history of Phototransistors status is:')
+            for status in self.report_sensor_status:
+                print(status)
+        elif self.device_type == 'hygrometer':
+            print('The history of hygrometers status is:')
+            for status in self.report_sensor_status:
+                print(status)
+
 
 
 
@@ -140,14 +186,22 @@ if __name__=='__main__':
     #print(b.group)
     #print(a.name)
     #print(b.name)
-    c = Sensor('home/garden/lights/lamp1')
-    d = Sensor('home/garden/waters/spray1')
+    c = Sensor('home/garden/Phototransistors/P1')
+    d = Sensor('home/garden/hygrometer/H1')
+    #print(c.group)
+    #print(d.group)
+    #print(c.name)
+    #print(d.name)
     c.read_sensor()
     d.read_sensor()
-    #a.turn_on()
-    #b.turn_off()
+    a.turn_on()
+    b.turn_off()
     b.humidity_check()
     a.controlling_light_time()
-    #a.get_status()
-    #b.get_status()
+    a.get_status()
+    b.get_status()
+    c.turn_on()
+    d.turn_off()
+
+
 
---
+++ finalproject_Task2,3.py
@@ -0,0 +1,330 @@
+from finalproject_Task1 import *
+
+class AdminPanel:
+
+    def __init__(self):
+        self.groups = {}
+        self.devices={}
+        self.username=[]
+        self.password= None
+        self.status_in_group={}
+        self.status_in_device_type=[]
+        self.sensor_groups={}
+        self.energy_threshold = 400
+
+
+
+
+    def set_password(self):
+        Syms =['$', '@', '#', '%']
+        username= input('Please Enter your username :')
+        if username not in self.username:
+            self.username.append(username)
+        while True:
+            new_password = input('Please set a password :')
+            if len(new_password.strip()) < 8 :
+                print('Your password is weak! Please try again.')
+            elif  not any(char.isdigit() for char in new_password):
+                print('Password should have at least one numeral')
+            elif not any(char.isupper() for char in new_password):
+                print('Password should have at least one uppercase letter')
+            elif not any(char.islower() for char in new_password):
+                print('Password should have at least one lowercase letter')
+            elif not any(char in Syms for char in new_password):
+                print('Password should have at least one of the symbols $@#')
+            else:
+                confirm_pass = input('Please confirm your password :')
+                if confirm_pass == new_password:
+                    self.password = new_password
+                    print('You have successfully set your username and password!!')
+                    return
+
+                else:
+                    print('Entered passwords do not match. Please try again.')
+
+
+
+    def create_group(self, group_name):
+        if not self.check_info():
+            print("Access denied. Can not create group.")
+            return
+
+        if group_name not in self.groups:
+            self.groups[group_name] = []
+            print(f'Group {group_name} is created')
+
+        else:
+            print('Your group name has already existed !')
+
+
+    def add_device_to_group(self, group_name, device):
+        if group_name in self.groups:
+            self.groups[group_name].append(device)
+
+        else:
+            print(f'Group {group_name} does not exist')
+
+
+
+
+    def create_device(self, group_name, device_type, name):
+        if not self.check_info():
+            print("Access denied. Can not create device.")
+            return
+        if group_name in self.groups:
+            topic = f'home/{group_name}/{device_type}/{name}'
+            new_device = Device(topic)
+            self.add_device_to_group(group_name, new_device)
+            print(f'The new device : {new_device} created !')
+        else:
+            print(f'Group {group_name} does not exist')
+
+
+
+
+
+    def create_multiple_devices(self, group_name, device_type, number_of_devices):
+
+        if group_name in self.groups:
+            for i in range(1, number_of_devices + 1):
+                device_name = f"{device_type}{i}"
+                topic = f'home/{group_name}/{device_type}/{device_name.lower()}'
+                new_device = Device(topic)
+                self.add_device_to_group(group_name, new_device)
+            print(f'{number_of_devices} devices were created !')
+        else:
+            print(f'Group {group_name} does not exist')
+
+    def get_devices_in_groups(self, group_name):
+        if group_name in self.groups:
+            return self.groups[group_name]
+
+        else:
+            print(f'Group {group_name} does not exist')
+            return []
+
+
+
+
+
+
+
+
+    def turn_on_all_in_group(self, group_name):
+
+        if not self.check_info():
+            print("Access denied.")
+            return
+        devices = self.groups[group_name]
+        for device in devices:
+            device.turn_on()
+        print(f'All Devices in {group_name} is turned on !')
+
+
+
+
+    def turn_off_all_in_group(self, group_name):
+        if not self.check_info():
+            print("Access denied.")
+            return
+        devices = self.groups[group_name]
+        for device in devices:
+            device.turn_off()
+        print(f'All Devices in {group_name} is turned on !')
+
+
+
+
+
+    def turn_on_all(self):
+        if not self.check_info():
+            print("Access denied. Can not turn on.")
+            return
+        for device in self.groups.values():
+            device.turn_on()
+        print('All Devices are turned on successfully !')
+
+
+
+
+    def turn_off_all(self):
+        if not self.check_info():
+            print("Access denied. Can not turn off.")
+            return
+        for device in self.groups.values():
+            device.turn_off()
+        print('All Devices turned on successfully !')
+
+
+
+    def get_status_in_group(self, group_name):
+        if not self.check_info():
+            print("Access denied. Can not retrieve group status.")
+            return
+        status_report = []
+        devices= self.groups[group_name]
+        if group_name in self.groups:
+            for device in devices:
+                device_status = {
+                    'Device_Name': device.name,
+                    'Status': device.status
+                }
+                status_report.append(device_status)
+
+
+
+
+            print(f'The report of devices defined in {group_name}  is as follows:')
+            print(f"Group: {group_name}")
+            for device_status in status_report:
+                print(f" Device Name: {device_status['Device_Name']}, Status: {device_status['Status']}")
+
+        else:
+            print(f'Group "{group_name}" does not exist!')
+
+
+
+    def get_status_in_device_type(self, device_type):
+        if not self.check_info():
+            print("Access denied. Can not retrieve device status.")
+            return
+        print('The report of selected device_type in different groups defined in the house is as follows:')
+        for devices in self.groups.values():
+            for device in devices:
+                if device.device_type == device_type:
+                    print(f"Device Name: {device.name}, Status: {device.status}")
+
+
+
+
+
+    def create_sensor(self, group_name, device_type, name):
+        if not self.check_info():
+            print("Access denied. Can not create sensor.")
+            return
+        if group_name in self.groups:
+            topic = f'home/{group_name}/{device_type}/{name}'
+            new_sensor = Sensor(topic)
+            self.add_sensor_to_group(group_name, new_sensor)
+            print(f'The new sensor : {new_sensor} created !')
+        else:
+            print(f'Group {group_name} does not exist')
+
+
+
+    def add_sensor_to_group(self, group_name, sensor):
+        if group_name in self.groups:
+            self.groups[group_name].append(sensor)
+
+        else:
+            print(f'Group {group_name} does not exist !')
+
+
+
+    def create_multiple_sensor(self, group_name, device_type, number_of_sensor):
+        if group_name in self.groups:
+            for i in range(1, number_of_sensor + 1):
+                sensor_name = f" {device_type}{i}"
+                topic = f'home/{group_name}/{device_type}/{sensor_name.lower()}'
+                new_sensor = Sensor(topic)
+                self.add_sensor_to_group(group_name, new_sensor)
+            print(f'{number_of_sensor} sensors were created !')
+        else:
+            print(f'Group {group_name} does not exist !')
+
+
+    def active_sensor(self, group_name, device_type):
+        if not self.check_info():
+            print("Access denied. Can not Add sensor to group.")
+            return
+        if group_name in self.groups:
+            for sensor in self.groups[group_name]:
+                if device_type == ' Phototransistors':
+                    sensor.turn_on()
+
+                elif device_type == 'hygrometer':
+                    sensor.turn_on()
+            print(f'The all sensors of type: {device_type} were turned on !')
+        else:
+            print(f'Group {group_name} does not exist !')
+
+    def deactivate_sensor(self, group_name, device_type):
+        if not self.check_info():
+            print("Access denied. Can not Add sensor to group.")
+            return
+        if group_name in self.groups:
+            for sensor in self.groups[group_name]:
+                if device_type == 'Phototransistors':
+                    sensor.turn_off()
+
+                elif device_type == 'hygrometer':
+                    sensor.turn_off()
+            print(f'The all sensors of type: {device_type} were turned off!')
+        else:
+            print(f'Group {group_name} does not exist !')
+
+
+
+
+    def check_info(self):
+        for i in range(3):
+            username= input('Please Enter your username :')
+            if username in self.username:
+                logging_password = input('Please Enter your password :')
+                if logging_password != self.password:
+                    print('Your entered password is not correct!')
+                else:
+                    print('Login successful!')
+                    return True
+
+            else:
+                print("Username does not exist!")
+
+        print('Your AdminPanel is locked due to too many failed attempts!')
+        return False
+
+
+    def energy_control(self):
+        total_energy=0
+        for devices in self.groups.values():
+            for device in devices:
+                if device.device_type == 'lights' and device.status == 'ON':
+                    energy_consumption= device.power_rating * device.hours_on
+                    total_energy += energy_consumption
+        return total_energy
+
+    def send_alarm(self):
+        total_energy= self.energy_control()
+        if total_energy > self.energy_threshold:
+            print(f'ALARM! Energy consumption exceeded threshold! Total: {total_energy} watt-hours')
+        else:
+            print('*** Energy consumption is optimal ! ***')
+
+
+
+
+
+
+
+
+
+if __name__=='__main__':
+    a=AdminPanel()
+    a.set_password()
+    a.create_group('garden')
+    a.create_group('kitchen')
+    a.create_device('garden','lights', 'lamp0')
+    a.create_device('kitchen', 'lights', 'lamp0')
+    a.create_device('garden', 'waters', 'spray0')
+    a.create_multiple_devices('garden', 'lights', 10)
+    a.turn_on_all_in_group('garden')
+   #a.energy_control()
+   #a.send_alarm()
+    a.create_sensor('garden', 'hygrometer', 'H1')
+    a.create_multiple_sensor('garden','hygrometer',5)
+    a.active_sensor('garden', 'hygrometer')
+    a.get_status_in_group('garden')
+    a.get_status_in_group('kitchen')
+   #a.get_status_in_device_type('lights')
+   #a.get_status_in_device_type('waters')
+   #a.get_status_in_device_type('hygrometer')
